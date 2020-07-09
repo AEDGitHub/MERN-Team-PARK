529510c7ed4082e676eee15c52e06339d6acd03f
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-07-09 16:45:06 -0700

    Adds styling to main

--- frontend/src/components/groups/join_group_form.jsx
+++ frontend/src/components/groups/join_group_form.jsx
@@ -23,14 +23,29 @@ class JoinGroupForm extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    onChange={this.handleInput}
-                    required
-                />
-                <button>Join Group</button>
-            </form>
+            <div className="row">
+                <form className="col s12" onSubmit={this.handleSubmit}>
+
+                    <h4 className="center">Join a Group</h4>
+
+                    <div className="row">
+                        <div className="input-field col s8 m6 l8 offset-l2 offset-m3 offset-s2">
+                            <input
+                                type="text"
+                                onChange={this.handleInput}
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col s8 m6 l8 offset-l2 offset-m3 offset-s2">
+                            <button className="btn col">Join Group</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
         )
     }
 
