b6e9feb938da1be00c65dccc1de3a9983fe85c27
Ben Rose <benrose207@gmail.com>
2020-07-08 22:07:49 -0700

    Add basic group join form to group index

--- frontend/src/components/groups/join_group_form.jsx
+++ frontend/src/components/groups/join_group_form.jsx
@@ -7,12 +7,29 @@ class JoinGroupForm extends React.Component {
         this.state = {
             slug: ""
         }
+
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(e) {
+        this.setState({ slug: e.currentTarget.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.joinGroup(this.state.slug)
     }
 
     render() {
         return (
-            <form>
-                
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    onChange={this.handleInput}
+                    required
+                />
+                <button>Join Group</button>
             </form>
         )
     }
