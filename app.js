59f3c91608f1cdefc02383f65a6786dbbecda510
ElishaPark <elishaspark@gmail.com>
2020-07-08 20:31:58 -0700

    Adds route to groups, begins events

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const users = require("./routes/api/users");
 const groups = require("./routes/api/groups");
 const interests = require("./routes/api/interests");
+const events = require("./routes/api/events");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
@@ -17,7 +18,7 @@ app.use("/api/users", users);
 app.use("/api/users", users);
 app.use("/api/groups", groups);
 app.use("/api/interests", interests);
-
+// app.use("/api/events", events);
 app.get("/", (req, res) => res.send("Hello World"));
 
 const db = require("./config/keys").mongoURI;
