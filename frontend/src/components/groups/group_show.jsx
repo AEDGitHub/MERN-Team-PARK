ce917985f48742c89278df5cb70e67d6ea63ad86
Ben Rose <benrose207@gmail.com>
2020-07-09 20:22:02 -0700

    Add interests to GroupShow component

--- frontend/src/components/groups/group_show.jsx
+++ frontend/src/components/groups/group_show.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import InterestFeed from "../user_profile/interest_feed";
 
 class GroupShow extends React.Component {
     componentDidMount() {
@@ -6,8 +7,20 @@ class GroupShow extends React.Component {
     }
 
     render() {
+        const sharingLink =
+          this.props.currentUserId === this.props.group.owner ? (
+            <div>
+              <p>
+                Invite other people to this group using the following link: 
+                {`https://rebond.herokuapp.com/#/signup?group=${this.props.group.slug}`}
+              </p>
+            </div>
+          ) : null;
         return (
-            <div></div>
+            <div>
+                {sharingLink}
+                <InterestFeed interests={this.props.interests} />
+            </div>
         )
     }
 }
