0547f5c6e8f337e2ac0a2d2f03e438709ac71d5f
Ben Rose <benrose207@gmail.com>
2020-07-20 17:56:24 -0700

    change interest layout based on parent component

--- frontend/src/components/user_profile/interest_feed_item.jsx
+++ frontend/src/components/user_profile/interest_feed_item.jsx
@@ -3,32 +3,32 @@ import React from 'react';
 class InterestFeedItem extends React.Component {
     render() {
       const { interest, currentUserId, followInterest, parentContainer } = this.props;
-      const followButton =
-        currentUserId !== interest.owner ? (
-          // <button onClick={() => followInterest(interest._id)}>Follow</button>
-          <a
-            className="btn-floating halfway-fab waves-effect waves-light teal"
+
+      const followButton = currentUserId !== interest.owner ? (
+          <button
+            className="interest-action"
             onClick={() => followInterest(interest._id)}
           >
             {interest.users.includes(currentUserId) ? <i className="fa fa-heart"></i> : <i className="fa fa-heart-o"></i>}
-          </a>
+          </button>
         ) : null;
     
-      const cardImage = (parentContainer ? (
-        <div className="card-image">
-          {/* <img src="" alt=""/> */}
-          <div className="default-interest-icon">
-            <i className="fa fa-cutlery"></i>
-          </div>
+      const cardImageElements = (parentContainer ? (
+        <>
           <span className="interest-title">{interest.name}</span>
           {followButton}
-        </div>
+        </>
       ) : null)
 
       return (
-        <li className="interest-container col l6 m6 s12">
-          <div className="interest-holder">
-            {cardImage}
+        <li className={`interest-container ${parentContainer ? "two-column" : "one-column"}`}>
+          <div className={`interest-holder${parentContainer ? "" : " horizontal"}`}>
+            <div className="card-image">
+              <div className="default-interest-icon">
+                <i className="fa fa-cutlery"></i>
+              </div>
+              {cardImageElements}
+            </div>
             <div className="interest-content">
               {parentContainer ? null : <span className="interest-title">{interest.name}</span>}
               <p>{interest.category}</p>
