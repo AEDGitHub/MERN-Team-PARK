68ac9458efbfb25e447179c9a8ddcaa610f82870
Ben Rose <benrose207@gmail.com>
2020-07-23 10:05:55 -0700

    clean up user photo update feature

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -2,6 +2,7 @@ import {
     RECEIVE_USER
 } from "../actions/user_actions";
 import { RECEIVE_GROUP_JOIN, RECEIVE_GROUP } from "../actions/group_actions";
+import { arrayBufferToBase64 } from "../util/photo_util";
 
 const usersReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
@@ -9,6 +10,13 @@ const usersReducer = (oldState = {}, action) => {
     switch (action.type) {
         case RECEIVE_USER:
             let receivedUser = action.user.data || action.user;
+
+            if (receivedUser.img) {
+                const base64Flag = `data:${receivedUser.img.contentType};base64,`;
+                const imageStr = arrayBufferToBase64(receivedUser.img.data.data);
+                receivedUser.img = base64Flag + imageStr;
+            }
+
             nextState[receivedUser._id] = receivedUser;
             return nextState
         case RECEIVE_GROUP_JOIN:
