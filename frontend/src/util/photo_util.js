d49fbf8d38f77083a222132f32675b9aec2097c1
Ben Rose <benrose207@gmail.com>
2020-07-22 21:33:10 -0700

    add photo upload feature for profile pictures

---
+++ frontend/src/util/photo_util.js
@@ -0,0 +1,6 @@
+export const arrayBufferToBase64 = (buffer) => {
+    let binary = '';
+    const bytes = [].slice.call(new Uint8Array(buffer));
+    bytes.forEach((b) => binary += String.fromCharCode(b));
+    return window.btoa(binary);
+};
