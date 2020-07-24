5bde2da35e071a849d3d528c92c96f5befd76519
AED <arndtericdaniel@gmail.com>
2020-07-24 10:39:26 -0700

    Transfers photo logic from users reducer to photo util file

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -2,7 +2,7 @@ import {
     RECEIVE_USER
 } from "../actions/user_actions";
 import { RECEIVE_GROUP_JOIN, RECEIVE_GROUP } from "../actions/group_actions";
-import { arrayBufferToBase64 } from "../util/photo_util";
+import { processUserPhoto } from "../util/photo_util";
 
 const usersReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
@@ -10,25 +10,23 @@ const usersReducer = (oldState = {}, action) => {
     switch (action.type) {
         case RECEIVE_USER:
             let receivedUser = action.user.data || action.user;
-
-            if (receivedUser.img) {
-                const base64Flag = `data:${receivedUser.img.contentType};base64,`;
-                const imageStr = arrayBufferToBase64(receivedUser.img.data.data);
-                receivedUser.img = base64Flag + imageStr;
-            }
-
+            processUserPhoto(receivedUser);
             nextState[receivedUser._id] = receivedUser;
-            return nextState
+            return nextState;
         case RECEIVE_GROUP_JOIN:
             let { user } = action.payload.data;
+            processUserPhoto(user);
             nextState[user._id] = user;
             return nextState;
         case RECEIVE_GROUP:
             if (action.payload.data.user) {
-                nextState[action.payload.data.user._id] = action.payload.data.user;
+                let receivedUser = action.payload.data.user;
+                processUserPhoto(receivedUser);
+                nextState[action.payload.data.user._id] = receivedUser;
             } else {
                 action.payload.data.users.forEach(user => {
-                    nextState[user._id] = user
+                    processUserPhoto(user);
+                    nextState[user._id] = user;
                 })
             }
             return nextState;
--- frontend/src/util/photo_util.js
+++ frontend/src/util/photo_util.js
@@ -1,6 +1,17 @@
-export const arrayBufferToBase64 = (buffer) => {
+const arrayBufferToBase64 = (buffer) => {
     let binary = '';
     const bytes = [].slice.call(new Uint8Array(buffer));
     bytes.forEach((b) => binary += String.fromCharCode(b));
     return window.btoa(binary);
 };
+
+export const processUserPhoto = (user) => {
+    if (user.img) {
+        const base64Flag = `data:${user.img.contentType};base64,`;
+        const imageStr = arrayBufferToBase64(user.img.data.data);
+        user.img = base64Flag + imageStr;
+    }
+}
+
+
+
