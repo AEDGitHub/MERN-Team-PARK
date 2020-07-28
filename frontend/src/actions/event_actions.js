9c019c37df0fb59b5b0c5f328387ca26a74a5d40
Ben Rose <benrose207@gmail.com>
2020-07-28 10:54:08 -0700

    add events reducer and unjoin event actions

--- frontend/src/actions/event_actions.js
+++ frontend/src/actions/event_actions.js
@@ -1,11 +1,27 @@
 import * as APIUtil from "../util/event_api_util";
 
-export const RECEIVE_EVENT = "RECEIVE_EVENT";
+export const RECEIVE_USER_EVENT = "RECEIVE_USER_EVENT";
+export const RECEIVE_JOINED_EVENT = "RECEIVE_JOINED_EVENT";
+export const RECEIVE_UNJOINED_EVENT = "RECEIVE_UNJOINED_EVENT";
 export const RECEIVE_EVENTS = "RECEIVE_EVENTS";
 
-const receiveEvent = event => {
+const receiveUserEvent = event => {
     return {
-        type: RECEIVE_EVENT,
+        type: RECEIVE_USER_EVENT,
+        event
+    };
+};
+
+const receiveJoinedEvent = event => {
+    return {
+        type: RECEIVE_JOINED_EVENT,
+        event
+    };
+};
+
+const receiveUnjoinedEvent = event => {
+    return {
+        type: RECEIVE_UNJOINED_EVENT,
         event
     };
 };
@@ -19,17 +35,22 @@ const receiveEvents = events => {
 
 export const createEvent = eventData => dispatch => {
     return APIUtil.createEvent(eventData)
-        .then(event => dispatch(receiveEvent(event)));
-};
-
-export const joinEvent = eventID => dispatch => {
-    return APIUtil.joinEvent(eventID)
-        .then(event => dispatch(receiveEvent(event)));
+        .then(event => dispatch(receiveUserEvent(event)));
 };
 
 export const editEvent = eventData => dispatch => {
     return APIUtil.editEvent(eventData)
-        .then(event => dispatch(receiveEvent(event)));
+        .then(event => dispatch(receiveUserEvent(event)));
+};
+
+export const joinEvent = eventID => dispatch => {
+    return APIUtil.joinEvent(eventID)
+        .then(event => dispatch(receiveJoinedEvent(event)));
+};
+
+export const unjoinEvent = eventID => dispatch => {
+    return APIUtil.unjoinEvent(eventID)
+        .then(event => dispatch(receiveUnjoinedEvent(event)));
 };
 
 export const fetchUserEvents = userID => dispatch => {
--- frontend/src/util/event_api_util.js
+++ frontend/src/util/event_api_util.js
@@ -4,12 +4,16 @@ export const createEvent = eventData => {
     return axios.post("/api/events", eventData);
 };
 
+export const editEvent = eventData => {
+    return axios.patch(`/api/events/${eventData.id}`, eventData);
+};
+
 export const joinEvent = eventID => {
     return axios.post(`/api/events/${eventID}/join`);
 };
 
-export const editEvent = eventData => {
-    return axios.patch(`/api/events/${eventData.id}`, eventData);
+export const unjoinEvent = eventID => {
+    return axios.post(`/api/events/${eventID}/unjoin`);
 };
 
 export const fetchUserEvents = userID => {
