9d7e30caf159344ac67c3d9243d9e13c8d36fe70
Ben Rose <benrose207@gmail.com>
2020-07-28 10:54:08 -0700

    add events reducer and unjoin event actions

--- frontend/src/reducers/events_reducer.js
+++ frontend/src/reducers/events_reducer.js
@@ -1,14 +1,40 @@
-import { RECEIVE_EVENT, RECEIVE_EVENTS } from "../actions/event_actions";
+import {
+    RECEIVE_USER_EVENT,
+    RECEIVE_JOINED_EVENT,
+    RECEIVE_UNJOINED_EVENT,
+    RECEIVE_EVENTS
+} from "../actions/event_actions";
 
 const eventsReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
 
     switch (action.type) {
-        case RECEIVE_EVENT:
-            nextState[action.event._id] = action.event;
+        case RECEIVE_USER_EVENT:
+            nextState.createdEvents[action.event._id] = action.event;
+        case RECEIVE_JOINED_EVENT:
+            delete nextState.invitedEvents[action.event._id]; // remove event from invited slice of event state
+            nextState.confirmedEvents[action.event._id] = action.event; // add to confirmed slice of event state
+        case RECEIVE_UNJOINED_EVENT:
+            delete nextState.confirmedEvents[action.event._id]; // remove event from confirmed slice of event state
+            nextState.invitedEvents[action.event._id] = action.event; // add to invited slice of event state
         case RECEIVE_EVENTS:
-            // Need to figure out what data from backend looks like first before adding
+            // put all created events in their own slice of event state
+            action.events.ownedEvents.forEach(event => {
+                nextState.createdEvents[event._id] = event;
+            });
+
+            // Add confirmed events to their own slice of event state. Skip if they are already in created
+            action.events.confirmedEvents.forEach(event => {
+                if (event._id in nextState.createdEvents) return;
+                nextState.confirmedEvents[event._id] = event;
+            })
+
+            // Add invited events to their own slice of event state. Skip if they are already in created or confirmed
+            action.events.invitedEvents.forEach(event => {
+                if (event._id in nextState.createdEvents || event._id in nextState.confirmedEvents) return;
+                nextState.invitedEvents[event._id] = event;
+            })
         default:
             return state;
     }
