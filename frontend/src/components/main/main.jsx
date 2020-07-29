9fa3b300eeda490e40fa85ccb60fb63286bb0718
AED <arndtericdaniel@gmail.com>
2020-07-29 10:30:43 -0700

    Adds container class to main content area

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import UserProfileContainer from '../user_profile/user_profile_container';
+import React from "react";
+import UserProfileContainer from "../user_profile/user_profile_container";
 import GroupIndexContainer from "../groups/group_index_container";
 
 class Main extends React.Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="main-left-container">
+          <UserProfileContainer />
+        </div>
 
-    render() {
-        return (
-            <div className="row">
-
-                <div className="main-left-container">
-                    <UserProfileContainer />
-                </div>
-
-                <div className="main-right-container">
-                    <GroupIndexContainer />
-                </div>
-                    
-            </div>
-        )
-    }
+        <div className="main-right-container">
+          <GroupIndexContainer />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Main;
