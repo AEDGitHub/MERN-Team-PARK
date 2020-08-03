e1c334cfabe6ac87f26580a64ad47ba44a74c0b8
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-08-02 17:09:41 -0700

    Fix event list

--- frontend/src/components/events/event_feed.jsx
+++ frontend/src/components/events/event_feed.jsx
@@ -13,8 +13,20 @@ class EventFeed extends React.Component {
         //     swipeable: true
         // }
         this.props.fetchUserEvents(this.props.currentUserId)
+            .then(this.attachTabHandlers())
             // .then(M.Tabs.init(this.Tabs))
 
+        // M.Tabs.init(this.Tabs)
+    }
+
+    componentDidUpdate(prevProps) {
+        debugger
+        if (prevProps.events !== this.props.events) {
+            this.attachTabHandlers();
+        }
+    }
+
+    attachTabHandlers() {
         M.Tabs.init(this.Tabs)
     }
 
@@ -26,8 +38,8 @@ class EventFeed extends React.Component {
             // the EventFeedItemComponent is not needed if you use this
             // debugger
             return (
-                <li className="event-list-element" key={event._id}>
-                    <a href={`#event-swipe-${idx}`}>
+                <li className="tab col s3" key={idx}>
+                    <a href={`#test-swipe-${idx + 1}`}>
                         {event.name}
                     </a>
                 </li>
@@ -37,7 +49,7 @@ class EventFeed extends React.Component {
         const ownedEvents = Object.values(this.props.events.createdEvents).map((event, idx) => {
             // the EventFeedItemComponent is not needed if you use this
             return (
-                <div id={`event-swipe-${idx}`} className="col s12" key={event._id}>
+                <div id={`test-swipe-${idx + 1}`} className="col s12 red" key={idx}>
                     {event.name}
                 </div>
             )
@@ -83,33 +95,64 @@ class EventFeed extends React.Component {
             //     </div>
             // </div>
             <>
-                <ul
-                    ref={Tabs => {
-                        this.Tabs = Tabs;
-                    }}
-                    id="tabs-swipe-demo"
-                    class="tabs"
-                >
-                    <li class="tab col s3">
-                        <a href="#test-swipe-1">Test 1</a>
-                    </li>
-                    <li class="tab col s3">
-                        <a href="#test-swipe-2">Test 2</a>
-                    </li>
-                    <li class="tab col s3">
-                        <a href="#test-swipe-3">Test 3</a>
-                    </li>
-                </ul>
+                <div clasName="row">
+                    <div className="col l12 m12 s12">
+                        <ul
+                            ref={Tabs => {
+                                this.Tabs = Tabs;
+                            }}
+                            id="tabs-swipe-demo"
+                            class="tabs tabs-fixed-width tab-demo z-depth-1"
+                        >
 
-                <div id="test-swipe-1" class="col s12 blue">
-                    Test 1
-        </div>
-                <div id="test-swipe-2" class="col s12 red">
-                    Test 2
-        </div>
-                <div id="test-swipe-3" class="col s12 green">
-                    Test 3
-        </div>
+                            <li className="tab col s3">
+                                <a href="#test-swipe-0">UPCOMING EVENTS</a>
+                            </li>
+                            {ownedEventsTabs}
+                            {/* <li class="tab col s3">
+                                 <a href="#test-swipe-1">Test 1</a>
+                            </li>
+                            <li class="tab col s3">
+                                <a href="#test-swipe-2">Test 2</a>
+                            </li>
+                            <li class="tab col s3">
+                                <a href="#test-swipe-3">Test 3</a>
+                            </li>
+                            <li class="tab col s3">
+                                <a href="#test-swipe-4">Test 4</a>
+                            </li>
+                            <li class="tab col s3">
+                                <a href="#test-swipe-5">Test 5</a>
+                            </li>
+                            <li class="tab col s3">
+                                <a href="#test-swipe-6">Test 6</a>
+                            </li> */}
+                        </ul>
+
+                        <div id="test-swipe-0" class="col s12 yellow">
+                            UPCOMING EVENTS
+                        </div>
+                        {ownedEvents}
+                            {/* <div id="test-swipe-1" class="col s12 blue">
+                                Test 1
+                            </div>
+                            <div id="test-swipe-2" class="col s12 red">
+                                Test 2
+                            </div>
+                            <div id="test-swipe-3" class="col s12 green">
+                                Test 3
+                            </div>
+                            <div id="test-swipe-4" class="col s12 blue">
+                                Test 4
+                            </div>
+                            <div id="test-swipe-5" class="col s12 red">
+                                Test 5
+                            </div>
+                            <div id="test-swipe-6" class="col s12 green">
+                                Test 6
+                            </div> */}
+                        </div>
+                </div>
             </>
         )
     }
