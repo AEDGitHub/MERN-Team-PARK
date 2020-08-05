d45c5de7032a5279e758cbd36a5ac17e73999851
Ben Rose <benrose207@gmail.com>
2020-08-05 14:54:08 -0700

    implement basic sidenav on smaller devices

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import M from "materialize-css";
 
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
         this.logoutUser = this.logoutUser.bind(this);
         this.getLinks = this.getLinks.bind(this);
+    }
+
+    componentDidMount() {
+        const sideNav = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(sideNav);
     }
 
     logoutUser(e) {
@@ -19,7 +25,7 @@ class NavBar extends React.Component {
         
         if (loggedIn) {
             return (
-                <ul className="navbar-session-button-holder">
+                <>
                     <li><Link to={'/about'}>About</Link></li>
                     {hasGroups ? <li><Link to={'/main'}>Groups</Link></li> : null}
                     {hasGroups ? <li><Link to={'/main/events'}>Events</Link></li> : null}
@@ -30,25 +36,34 @@ class NavBar extends React.Component {
                             Logout
                         </button>
                     </li>
-                </ul>
+                </>
             );
         } else {
             return (
-                <ul className="navbar-session-button-holder">
+                <>
                     <li><Link to={'./about'}>About</Link></li>
                     <li><Link to={'/signup'}>Sign Up</Link></li>
                     <li><Link to={'/login'}>Login</Link></li>
-                </ul>
+                </>
             );
         }
     }
 
     render() {
         return (
-            <nav className="navbar-wrapper">
-                <a className="navbar-brand-logo" href="/">ReBond</a>
-                {this.getLinks()}
-            </nav>
+            <>
+                <nav className="navbar-wrapper">
+                    <a className="navbar-brand-logo" href="/">ReBond</a>
+                    <a href="#" data-target="mobile-nav-menu" className="sidenav-trigger right"><i className="material-icons">menu</i></a>
+                    <ul className="navbar-session-button-holder">
+                        {this.getLinks()}
+                    </ul>
+                </nav>
+
+                <ul className="sidenav right" id="mobile-nav-menu">
+                    {this.getLinks()}
+                </ul>
+            </>
         );
     }
 }
