4805f54e28e1559e92cb91f6466a485ee18710d6
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-08-12 14:38:42 -0700

    Update splash

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -3,14 +3,20 @@ import React from "react";
 class Splash extends React.Component {
   render() {
     return (
-      <div className="splash-page">
-        <img
-          //   className="responsive-img"
-          src="../../../images/splash_background_2.jpg"
-          alt="splash-background"
-        />
-        {/* <div className="splash-image"></div> */}
-      </div>
+      <>
+        <div className="splash-page">
+          <img
+            //   className="responsive-img"
+            src="../../../images/splash_background_2.jpg"
+            alt="splash-background"
+          /> 
+          {/* <div className="splash-image"></div> */}
+        </div>
+        <div className="splash-main-text">
+          <h2>Add interests. Join groups. Attend events.</h2>
+          <p>Sign up for the most innovative meetup app today for free.</p>
+        </div>
+      </>
     );
   }
 }
