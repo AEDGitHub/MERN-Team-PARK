15039a4ce57326390ebaf8380c8dc679312e1837
AED <arndtericdaniel@gmail.com>
2020-12-29 14:31:37 -0800

    Changes social media link on about page

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -1,239 +1,246 @@
-import React from "react";
+import React from "react"
 
 class About extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="about-general-container">
-          <h3 className="about-general-title">What is ReBond?</h3>
-          <p className="about-general-part">
-            In the shadow of COVID-19's influence, more groups of individuals
-            are forming on a strictly virtual basis than ever before. Acting
-            under the assumption that the virus will eventually loosen its grip
-            on humanity's collective behavior, these groups may eventually wish
-            to meet in a physical location. Although some of these meetings will
-            occur organically, it may be difficult for some to broach the
-            subject of an in-person interaction. After all, how does one ask to
-            'hang out' with a person they've known for months, but only through
-            a screen? Is that weird?
-          </p>
-          <p className="about-general-part">
-            ReBond seeks to solve this initial hesitance, by developing a
-            lightweight app to reduce the psychological friction of such
-            meetings occurring (once the virus subsides, of course.) The app is
-            not designed to supplant any existing method of social media, but
-            rather to supplement them during a time of nonstandard human
-            interaction: it's a meet-up app for people who already know each
-            other well, yet have never breathed the same air.
-          </p>
-        </div>
+	render() {
+		return (
+			<div>
+				<div className="about-general-container">
+					<h3 className="about-general-title">What is ReBond?</h3>
+					<p className="about-general-part">
+						In the shadow of COVID-19's influence, more groups of
+						individuals are forming on a strictly virtual basis than ever
+						before. Acting under the assumption that the virus will
+						eventually loosen its grip on humanity's collective behavior,
+						these groups may eventually wish to meet in a physical
+						location. Although some of these meetings will occur
+						organically, it may be difficult for some to broach the
+						subject of an in-person interaction. After all, how does one
+						ask to 'hang out' with a person they've known for months, but
+						only through a screen? Is that weird?
+					</p>
+					<p className="about-general-part">
+						ReBond seeks to solve this initial hesitance, by developing a
+						lightweight app to reduce the psychological friction of such
+						meetings occurring (once the virus subsides, of course.) The
+						app is not designed to supplant any existing method of social
+						media, but rather to supplement them during a time of
+						nonstandard human interaction: it's a meet-up app for people
+						who already know each other well, yet have never breathed the
+						same air.
+					</p>
+				</div>
 
-        <div className="about-team-container">
-          <h3 className="about-team-title">Meet the Team</h3>
-          <div className="about-engineer-row">
-            <div className="about-engineer-intro">
-              <img
-                src="../../../images/engineers/elisha.jpeg"
-                alt="elisha-kutnick profile"
-              />
-              <h5>Elisha (Park) Kutnick</h5>
-              <h6>Backend Lead</h6>
-              <p className="about-job-description">
-                As the Backend Lead, Elisha is responsible for the overall
-                functioning of the project and design of its schema, routes,
-                models and controllers, ensuring sensible processing of and
-                access to data on the backend, while correctly entering
-                user-submitted form data into database storage. The Backend Lead
-                has broad authority to dictate abstract data decisions, to
-                include naming of backend routes, design of app state shape, and
-                implementation of methods and validations as appropriate.
-              </p>
-              <div className="about-engineer-icons">
-                <a
-                  href="https://www.linkedin.com/in/elishakutnick/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/linkedin-icon.png"
-                    alt="linkedin icon"
-                  />
-                </a>
-                <a
-                  href="https://github.com/elishakutnick"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/github-icon.png"
-                    alt="github icon"
-                  />
-                </a>
-                <a
-                  href="https://angel.co/u/elishakutnick"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/angellist-icon.png"
-                    alt="angellist icon"
-                  />
-                </a>
-              </div>
-            </div>
-            <div className="about-engineer-intro">
-              <img
-                src="../../../images/engineers/gabor.jpeg"
-                alt="gabor-kurucz profile"
-              />
-              <h5>Gabor Kurucz</h5>
-              <h6>Frontend Lead</h6>
-              <p className="about-job-description">
-                As the Frontend Lead, Gabor is responsible for the overall
-                presentation of the project, to include styling, component
-                structure and function, and ensuring data is accurately
-                retrieved from and submitted to the backend. The Frontend Lead
-                has broad authority to dictate overall design decisions, to
-                include user interface/experience, naming of frontend routes,
-                component hierarchy, container design, reducer logic, and action
-                formulations.
-              </p>
-              <div className="about-engineer-icons">
-                <a
-                  href="https://www.linkedin.com/in/kurucz-gabor/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/linkedin-icon.png"
-                    alt="linkedin icon"
-                  />
-                </a>
-                <a
-                  href="https://github.com/kuruczgabor"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/github-icon.png"
-                    alt="github icon"
-                  />
-                </a>
-                <a
-                  href="https://angel.co/u/gaborkurucz"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/angellist-icon.png"
-                    alt="angellist icon"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
+				<div className="about-team-container">
+					<h3 className="about-team-title">Meet the Team</h3>
+					<div className="about-engineer-row">
+						<div className="about-engineer-intro">
+							<img
+								src="../../../images/engineers/elisha.jpeg"
+								alt="elisha-kutnick profile"
+							/>
+							<h5>Elisha (Park) Kutnick</h5>
+							<h6>Backend Lead</h6>
+							<p className="about-job-description">
+								As the Backend Lead, Elisha is responsible for the
+								overall functioning of the project and design of its
+								schema, routes, models and controllers, ensuring
+								sensible processing of and access to data on the
+								backend, while correctly entering user-submitted form
+								data into database storage. The Backend Lead has broad
+								authority to dictate abstract data decisions, to include
+								naming of backend routes, design of app state shape, and
+								implementation of methods and validations as
+								appropriate.
+							</p>
+							<div className="about-engineer-icons">
+								<a
+									href="https://www.linkedin.com/in/elishakutnick/"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/linkedin-icon.png"
+										alt="linkedin icon"
+									/>
+								</a>
+								<a
+									href="https://github.com/elishakutnick"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/github-icon.png"
+										alt="github icon"
+									/>
+								</a>
+								<a
+									href="https://angel.co/u/elishakutnick"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/angellist-icon.png"
+										alt="angellist icon"
+									/>
+								</a>
+							</div>
+						</div>
+						<div className="about-engineer-intro">
+							<img
+								src="../../../images/engineers/gabor.jpeg"
+								alt="gabor-kurucz profile"
+							/>
+							<h5>Gabor Kurucz</h5>
+							<h6>Frontend Lead</h6>
+							<p className="about-job-description">
+								As the Frontend Lead, Gabor is responsible for the
+								overall presentation of the project, to include styling,
+								component structure and function, and ensuring data is
+								accurately retrieved from and submitted to the backend.
+								The Frontend Lead has broad authority to dictate overall
+								design decisions, to include user interface/experience,
+								naming of frontend routes, component hierarchy,
+								container design, reducer logic, and action
+								formulations.
+							</p>
+							<div className="about-engineer-icons">
+								<a
+									href="https://www.linkedin.com/in/kurucz-gabor/"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/linkedin-icon.png"
+										alt="linkedin icon"
+									/>
+								</a>
+								<a
+									href="https://github.com/kuruczgabor"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/github-icon.png"
+										alt="github icon"
+									/>
+								</a>
+								<a
+									href="https://angel.co/u/gaborkurucz"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/angellist-icon.png"
+										alt="angellist icon"
+									/>
+								</a>
+							</div>
+						</div>
+					</div>
 
-          <div className="about-engineer-row">
-            <div className="about-engineer-intro">
-              <img
-                src="../../../images/engineers/ben.jpeg"
-                alt="benjamin-rose profile"
-              />
-              <h5>Benjamin Rose</h5>
-              <h6>Chief Deputy of Operations</h6>
-              <p className="about-job-description">
-                As the Deputy Ops, Ben is responsible for ensuring data flows
-                unhindered between the backend and the frontend, as well as
-                communicating problems requiring immediate decisions to the
-                Project Director. The Deputy Ops also assists with research for
-                the frontend and backend leads, as appropriate, and attempts to
-                prevent conflicts before they arise.
-              </p>
-              <div className="about-engineer-icons">
-                <a
-                  href="https://www.linkedin.com/in/benjamin-rose-483549124/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/linkedin-icon.png"
-                    alt="linkedin icon"
-                  />
-                </a>
-                <a
-                  href="https://github.com/benrose207"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/github-icon.png"
-                    alt="github icon"
-                  />
-                </a>
-                <a
-                  href="https://angel.co/u/benjamin-rose-2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/angellist-icon.png"
-                    alt="angellist icon"
-                  />
-                </a>
-              </div>
-            </div>
-            <div className="about-engineer-intro">
-              <img
-                src="../../../images/engineers/eric.jpeg"
-                alt="eric-arndt profile"
-              />
-              <h5>Eric Arndt</h5>
-              <h6>Project Director</h6>
-              <p className="about-job-description">
-                As the Project Director, Eric is responsible for the overall
-                management and support of the team, to include aligning the
-                vision, prioritizing tasks for execution in task management
-                software, allocating human resources to blockers as appropriate,
-                and overseeing the app version control and deployment.
-              </p>
-              <div className="about-engineer-icons">
-                <a
-                  href="https://www.linkedin.com/in/eric-arndt-9850281a5/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/linkedin-icon.png"
-                    alt="linkedin icon"
-                  />
-                </a>
-                <a
-                  href="https://github.com/AEDGitHub"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/github-icon.png"
-                    alt="github icon"
-                  />
-                </a>
-                <a
-                  href="https://angel.co/u/eric-daniel-arndt"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src="../../../images/icons/angellist-icon.png"
-                    alt="angellist icon"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+					<div className="about-engineer-row">
+						<div className="about-engineer-intro">
+							<img
+								src="../../../images/engineers/ben.jpeg"
+								alt="benjamin-rose profile"
+							/>
+							<h5>Benjamin Rose</h5>
+							<h6>Chief Deputy of Operations</h6>
+							<p className="about-job-description">
+								As the Deputy Ops, Ben is responsible for ensuring data
+								flows unhindered between the backend and the frontend,
+								as well as communicating problems requiring immediate
+								decisions to the Project Director. The Deputy Ops also
+								assists with research for the frontend and backend
+								leads, as appropriate, and attempts to prevent conflicts
+								before they arise.
+							</p>
+							<div className="about-engineer-icons">
+								<a
+									href="https://www.linkedin.com/in/benjamin-rose-483549124/"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/linkedin-icon.png"
+										alt="linkedin icon"
+									/>
+								</a>
+								<a
+									href="https://github.com/benrose207"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/github-icon.png"
+										alt="github icon"
+									/>
+								</a>
+								<a
+									href="https://angel.co/u/benjamin-rose-2"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/angellist-icon.png"
+										alt="angellist icon"
+									/>
+								</a>
+							</div>
+						</div>
+						<div className="about-engineer-intro">
+							<img
+								src="../../../images/engineers/eric.jpeg"
+								alt="eric-arndt profile"
+							/>
+							<h5>Eric Arndt</h5>
+							<h6>Project Director</h6>
+							<p className="about-job-description">
+								As the Project Director, Eric is responsible for the
+								overall management and support of the team, to include
+								aligning the vision, prioritizing tasks for execution in
+								task management software, allocating human resources to
+								blockers as appropriate, and overseeing the app version
+								control and deployment.
+							</p>
+							<div className="about-engineer-icons">
+								<a
+									href="https://www.linkedin.com/in/arndtericdaniel/"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/linkedin-icon.png"
+										alt="linkedin icon"
+									/>
+								</a>
+								<a
+									href="https://github.com/AEDGitHub"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/github-icon.png"
+										alt="github icon"
+									/>
+								</a>
+								<a
+									href="https://angel.co/u/eric-daniel-arndt"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src="../../../images/icons/angellist-icon.png"
+										alt="angellist icon"
+									/>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default About;
+export default About
